91f355d883ce0cbfb55165da956280eebe9394da
isimbinadege <isimbi0123@gmail.com>
2024-08-02 03:42:01 -0700

    Nwe changes

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,11 @@
+import Navbar from '@/components/app/Navbar'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      <h1>Hello World</h1>
-      <Button>Submit</Button>
+      <Navbar />
     </div>
   )
 }
