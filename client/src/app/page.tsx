14471eb9e14616c13c6a02d5f0ea49b2ff4e52b6
isimbinadege <isimbi0123@gmail.com>
2024-08-02 03:33:06 -0700

    Packages installation

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
       <h1>Hello World</h1>
+      <Button>Submit</Button>
     </div>
   )
 }
