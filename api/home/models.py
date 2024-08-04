e1585f13675fdb7b48ddc7060fcdef9965656b8d
uhiriwe <olivineuhiriwe2001@gmail.com>
2024-08-04 19:20:25 +0200

    Order table

--- api/home/models.py
+++ api/home/models.py
@@ -34,5 +34,9 @@ class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.profile} - {self.product}'
+
 
