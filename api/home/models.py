02c30b75e3df7fbc434590421f56e53344325d5b
uhiriwe <olivineuhiriwe2001@gmail.com>
2024-08-04 19:05:58 +0200

    Profile  Table

--- api/home/models.py
+++ api/home/models.py
@@ -18,3 +18,21 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name 
+    
+class Profile(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    email = models.EmailField(max_length=200)
+    address = models.CharField(max_length=200)
+    telephone = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+
