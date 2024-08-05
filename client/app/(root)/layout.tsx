a392e1cc240e5186806338f1ccc22f0d8c9679d0
uhiriwe <olivineuhiriwe2001@gmail.com>
2024-08-05 11:01:34 +0200

    Footer

--- client/app/(root)/layout.tsx
+++ client/app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/shared/Footer"
 import Navbar from "@/components/shared/Navbar"
 
 export default function RootLayout({
@@ -9,6 +10,7 @@ export default function RootLayout({
         <div className="flex h-screen flex-col">
             <Navbar />
             <main className="flex-1">{children}</main>
+            <Footer />
         </div>
     )
 }
