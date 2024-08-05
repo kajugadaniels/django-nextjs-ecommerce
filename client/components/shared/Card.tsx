bbb8039f9b5f52bfbf11cadc299c92e97324f85d
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-08-05 19:36:17 +0200

    Product Cart design

--- client/components/shared/Card.tsx
+++ client/components/shared/Card.tsx
@@ -1,26 +1,34 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const Card = () => {
     return (
-        <div className="relative group mb-10">
-            <div className="overflow-hidden aspect-w-1 aspect-h-1">
-                <img className="object-cover w-full h-full transition-all duration-300 group-hover:scale-125" src="https://i5.walmartimages.com/asr/a83e3e11-9128-4d98-8f6f-8c144e0d8e5e.a5fafdef89b7430bd13cae9037294d87.jpeg" alt="" />
-            </div>
-            <div className="flex items-start justify-between mt-4 space-x-4">
-                <div>
-                    <h3 className="text-xs font-bold text-gray-900 sm:text-sm md:text-base">
-                        <Link href="/shop/watermelon" title="">
-                            Watermelon
-                            <span className="absolute inset-0" aria-hidden="true"></span>
-                        </Link>
-                    </h3>
+        <div className='flex gap-x-4 gap-y-16 justify-between flex-wrap'>
+            <Link href='' className='w-full flex flex-col gap-5'>
+                <div className='relative w-full h-80'>
+                    <img
+                        src="https://i5.walmartimages.com/asr/a83e3e11-9128-4d98-8f6f-8c144e0d8e5e.a5fafdef89b7430bd13cae9037294d87.jpeg"
+                        alt=""
+                        sizes="25vw"
+                        className='absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500'
+                    />
+                    <img
+                        src="https://www.heynutritionlady.com/wp-content/uploads/2023/05/How_to_Cook_Kidney_Beans-SQ.jpg"
+                        alt=""
+                        sizes="25vw"
+                        className='absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500'
+                    />
                 </div>
-
-                <div className="text-right">
-                    <p className="text-xs font-bold text-gray-900 sm:text-sm md:text-base">$99.00</p>
+                <div className='flex justify-between'>
+                    <span className='font-medium'>Product Name</span>
+                    <span className='font-semibold'>$49</span>
                 </div>
-            </div>
+                <div className='text-sm text-gray-500'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, numquam.
+                </div>
+                <button className='rounded-2xl h-9 ring-1 ring-emerald-800 text-emerald-800 text-xs hover:bg-emerald-900 hover:text-white'>Add to Cart</button>
+            </Link>
         </div>
     )
 }
