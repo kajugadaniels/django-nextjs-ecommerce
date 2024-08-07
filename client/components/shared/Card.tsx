739c09d2109361fd7d35f52bb53a6aae7464a831
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-08-07 17:08:53 +0200

    Fix errors

--- client/components/shared/Card.tsx
+++ client/components/shared/Card.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from 'next/image';
 import Link from 'next/link';
@@ -58,8 +58,10 @@ const Card = () => {
                                 layout="fill"
                                 objectFit="cover"
                                 className='transition-transform duration-300 ease-in-out group-hover:scale-105'
+                                sizes="(max-width: 768px) 100vw, (min-width: 769px) 50vw"
                             />
                             <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-70">
+                                <div className="absolute inset-x-0 bottom-0 flex justify-between px-20 py-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
                                 <div className="absolute inset-x-0 bottom-0 flex justify-between px-20 py-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
                                     <button className="p-2 bg-white rounded-full shadow-lg transform transition-transform duration-700 ease-in-out hover:scale-110">
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -79,6 +81,7 @@ const Card = () => {
                                             <path fill="currentColor" d="M11.5 18c4 0 7.46-2.22 9.24-5.5C18.96 9.22 15.5 7 11.5 7s-7.46 2.22-9.24 5.5C4.04 15.78 7.5 18 11.5 18m0-12c4.56 0 8.5 2.65 10.36 6.5C20 16.35 16.06 19 11.5 19S3 16.35 1.14 12.5C3 8.65 6.94 6 11.5 6m0 2C14 8 16 10 16 12.5S14 17 11.5 17S7 15 7 12.5S9 8 11.5 8m0 1A3.5 3.5 0 0 0 8 12.5a3.5 3.5 0 0 0 3.5 3.5a3.5 3.5 0 0 0 3.5-3.5A3.5 3.5 0 0 0 11.5 9"/>
                                         </svg>
                                     </button>
+                                </div>
                                 </div>
                             </div>
                         </div>
