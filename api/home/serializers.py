24b23b69a77b828cd7fde93dfed167a0005a55c6
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-08-07 16:41:58 +0200

    Modify product serializers to retrieve multiple images

--- api/home/serializers.py
+++ api/home/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
-from .models import Product, Category,Profile,Order
+from .models import *
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ProductImage
+        fields = ['image']
+
+    def get_image(self, obj):
+        request = self.context.get('request')
+        if obj.image and request:
+            return request.build_absolute_uri(obj.image.url)
+        return None
 
 class ProductSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True, read_only=True)
+
     class Meta:
         model = Product
         fields = '__all__'
