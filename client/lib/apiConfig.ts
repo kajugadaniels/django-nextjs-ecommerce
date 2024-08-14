00259fdea208996457655f7b3402ef827e745931
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-08-14 19:24:39 +0200

    Client api config

---
+++ client/lib/apiConfig.ts
@@ -0,0 +1,26 @@
+// lib/apiConfig.ts
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const localApiUrl = 'http://127.0.0.1:8000/api';
+const productionApiUrl = 'https://ecommerce-api-pro.up.railway.app/api';
+
+const localMediaUrl = 'http://127.0.0.1:8000/media/';
+const productionMediaUrl = 'https://ecommerce-api-pro.up.railway.app/media/';
+
+export const getApiUrl = () => {
+    return isProduction ? productionApiUrl : localApiUrl;
+};
+
+export const getMediaUrl = () => {
+    return isProduction ? productionMediaUrl : localMediaUrl;
+};
+
+export const getPlaceholderImageUrl = () => {
+    return `${getMediaUrl()}placeholder.png`;
+};
+
+export const getProductsUrl = (category?: string) => {
+    const baseUrl = `${getApiUrl()}/products/`;
+    return category ? `${baseUrl}category/${category}/` : baseUrl;
+};
