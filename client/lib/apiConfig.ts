d3be91b01e89ac0f6e2c3836026304615422b54d
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-08-14 19:47:26 +0200

    Nwe changes

--- client/lib/apiConfig.ts
+++ client/lib/apiConfig.ts
@@ -1,9 +1,9 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
-const localApiUrl = 'http://127.0.0.1:8000/api';
+const localApiUrl = 'https://ecommerce-api-pro.up.railway.app/api';
 const productionApiUrl = 'https://ecommerce-api-pro.up.railway.app/api';
 
-const localMediaUrl = 'http://127.0.0.1:8000/media/';
+const localMediaUrl = 'https://ecommerce-api-pro.up.railway.app/media/';
 const productionMediaUrl = 'https://ecommerce-api-pro.up.railway.app/media/';
 
 export const getApiUrl = () => {
